03f481ae727feb76f502c62cdca275163ff6407a
Sven Seeberg <mail@sven-seeberg.de>
2021-04-26 20:25:21 +0200

    API: increase page size

--- src/core/settings.py
+++ src/core/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 50,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
